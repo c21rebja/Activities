e265894639d75dc4e269974b1181a6a9112dfdf1
c21rebja <c21rebja@student.his.se>
2022-04-12 15:13:07 +0200

    added logs for activity lifecycle to profile activity

--- app/src/main/java/com/example/activities/ProfileActivity.java
+++ app/src/main/java/com/example/activities/ProfileActivity.java
@@ -3,12 +3,39 @@ package com.example.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+        Log.d("===", "ProfileActivity resumed");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("===", "ProfileActivity started");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("===", "ProfileActivity stopped");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("===", "ProfileActivity destroyed");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        Log.d("===", "ProfileActivity created");
     }
 }
