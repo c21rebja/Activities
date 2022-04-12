4c44a34dc637288ac8b5497e46184d4e9612d985
c21rebja <c21rebja@student.his.se>
2022-04-12 14:59:19 +0200

    connected main acitivity button to profile activity

--- app/src/main/java/com/example/activities/MainActivity.java
+++ app/src/main/java/com/example/activities/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -46,6 +47,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Log.d("===", "Button has been pressed!!");
+                Intent intent = new Intent(MainActivity.this, ProfileActivity.class);
+                startActivity(intent);
             }
         });
     }
